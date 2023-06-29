1cf0dd4548dda229e5ec21068a73b426c37df9d2
rajath008 <106469287+rajath008@users.noreply.github.com>
2023-06-29 18:49:59 +0530

    Updated Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -93,7 +93,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-black to-gray-800 w-full h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full h-screen pt-10"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
